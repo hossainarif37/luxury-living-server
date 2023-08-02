5d7b0e82b7bcb382113347b6de3096f2f702f234
hossainarif37 <hossainarif4937@gmail.com>
2023-08-02 15:14:14 +0600

    completed all api

--- index.js
+++ index.js
@@ -7,9 +7,178 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 
-app.get('/users', (req, res) => {
-    res.send({ name: 'Md Arif', email: 'onexboyarif6833@gmail.com' })
-})
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ausoith.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const database = client.db('luxury-living');
+
+        //?********** Collections **********//
+        //User
+        const userCollection = database.collection('users');
+        // Service
+        const serviceCollection = database.collection('services');
+        // Cart
+        const cartCollection = database.collection('cart');
+        // Order
+        const orderCollection = database.collection('orders');
+
+        //* Verify the admin role
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.body.email;
+            const requestedUser = await userCollection.findOne({ email });
+            const isAdmin = requestedUser.role === 'admin';
+            if (!isAdmin) {
+                return res.send({ message: 'You have  no permission to make an admin' })
+            }
+            next();
+        }
+
+
+        //?-----------🤵 User Api Start 🤵-----------*//
+        //* POST a user
+        app.post('/users', async (req, res) => {
+            const doc = req.body;
+            const query = { email: doc.email };
+            const isUserExist = await userCollection.findOne(query);
+            if (isUserExist) {
+                return res.send({ message: 'User already exist!' })
+            }
+            const result = await userCollection.insertOne(doc);
+            res.send(result);
+        })
+
+        //* GET all users
+        app.get('/users', async (req, res) => {
+            const users = await userCollection.find({}).toArray();
+            res.send(users);
+        })
+
+        //* UPDATE the user role
+        app.patch('/users', verifyAdmin, async (req, res) => {
+            const filter = req.query;
+            const role = req.body.role;
+            const updateDoc = {
+                $set: {
+                    role: role
+                }
+            }
+
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+
+        })
+        //?-----------🤵 User Api End 🤵-----------*//
+
+        //?-----------🎀 Service Api Start 🎀-----------*//
+        //* POST a service
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        })
+
+        //* GET all services
+        app.get('/services', async (req, res) => {
+            const services = await serviceCollection.find({}).toArray();
+            res.send(services);
+        })
+
+        //*GET a specific service
+        app.get('/services/:id', async (req, res) => {
+            const query = req.params.id;
+            const result = await serviceCollection.findOne({ _id: new ObjectId(query) });
+            res.send(result);
+        })
+
+        //* UPDATE a specific service
+        app.put('/services', async (req, res) => {
+            const { id } = req.query;
+            const filter = { _id: new ObjectId(id) };
+            console.log(id);
+            const data = req.body;
+            const updateDoc = {
+                $set: data
+            }
+            const result = await serviceCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        //* DELETE a specific service
+        app.delete('/services', async (req, res) => {
+            const { id } = req.query;
+            const query = { _id: new ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+        //?-----------🎀 Service Api End 🎀-----------*//
+
+        //?-----------🛒 Cart Api Start 🛒-----------*//
+        //* POST a specific service to Cart
+        app.post('/cart', async (req, res) => {
+            const cartData = req.body;
+            const result = await cartCollection.insertOne(cartData);
+            res.send(result);
+        })
+
+        //* GET specific user cart
+        app.get('/cart', async (req, res) => {
+            const filter = req.query;
+            console.log(filter);
+            const cart = await cartCollection.find(filter).toArray();
+            res.send(cart);
+        })
+        //?-----------🛒 Cart Api End 🛒-----------*//
+
+        //?-----------🛒 Order Api Start 🛒-----------*//
+        //* POST a order
+        app.post('/orders', async (req, res) => {
+            const orderData = req.body;
+            const result = await orderCollection.insertOne(orderData);
+            res.send(result);
+        })
+
+        //* GET specific user orders
+        app.get('/orders', async (req, res) => {
+            const filter = req.query;
+            const orders = await orderCollection.find(filter).toArray();
+            res.send(orders);
+
+        })
+
+        //* GET all orders
+        app.get('/orders', async (req, res) => {
+            const orders = await orderCollection.find({}).toArray();
+            res.send(orders);
+        })
+
+
+
+
+        //?-----------🛒 Order Api End 🛒-----------*//
+
+
+        console.log("Database Connected");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Luxury Living Server Side')
